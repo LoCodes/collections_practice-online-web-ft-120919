20f5e14a4a98f0b259d67f91ee3e7b21502a6300
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-18 04:18:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,13 +38,13 @@ def find_a(array)
 end
 
 def sum_array(array)
-  sum = 0 
-   array.each do |num|
-    sum += num
+  sum = 0
+  array.each do |num|
+    sum+=num
   end
-  
   sum
 
+end
 
 def add_s(array)
   array.collect do |word|
